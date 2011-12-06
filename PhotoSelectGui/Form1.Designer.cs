71af442f5dd0073916ca52c9a7d1ed9f61c0d9ee
danielXr <daniel.braunstein@gmail.com>
2011-12-06 20:58:23 +0200

    1. fixed selected drive listener
    2. error message on driveSelect is now displayed in foldersList

--- PhotoSelectGui/Form1.Designer.cs
+++ PhotoSelectGui/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.components = new System.ComponentModel.Container();
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-            this.s1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
             this.rectangleShape2 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.s1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.folderslist = new System.Windows.Forms.ListBox();
             this.drivelist = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -52,29 +52,11 @@
             this.shapeContainer1.TabIndex = 0;
             this.shapeContainer1.TabStop = false;
             // 
-            // s1
-            // 
-            this.s1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.s1.BackColor = System.Drawing.Color.Maroon;
-            this.s1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.s1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
-            this.s1.BorderColor = System.Drawing.SystemColors.Desktop;
-            this.s1.CornerRadius = 8;
-            this.s1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.s1.FillColor = System.Drawing.Color.White;
-            this.s1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
-            this.s1.Location = new System.Drawing.Point(23, 118);
-            this.s1.Name = "s1";
-            this.s1.SelectionColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.s1.Size = new System.Drawing.Size(207, 50);
-            // 
             // rectangleShape2
             // 
-            this.rectangleShape2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.rectangleShape2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.rectangleShape2.BackColor = System.Drawing.Color.Maroon;
             this.rectangleShape2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.rectangleShape2.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
@@ -87,6 +69,24 @@
             this.rectangleShape2.Name = "rectangleShape2";
             this.rectangleShape2.SelectionColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.rectangleShape2.Size = new System.Drawing.Size(207, 50);
+            // 
+            // s1
+            // 
+            this.s1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.s1.BackColor = System.Drawing.Color.Maroon;
+            this.s1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.s1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.s1.BorderColor = System.Drawing.SystemColors.Desktop;
+            this.s1.CornerRadius = 8;
+            this.s1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.s1.FillColor = System.Drawing.Color.White;
+            this.s1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.s1.Location = new System.Drawing.Point(23, 118);
+            this.s1.Name = "s1";
+            this.s1.SelectionColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.s1.Size = new System.Drawing.Size(207, 50);
             // 
             // groupBox1
             // 
@@ -101,6 +101,19 @@
             this.groupBox1.Size = new System.Drawing.Size(523, 377);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("Miriam", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(419, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Done";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // folderslist
             // 
@@ -117,19 +130,7 @@
             this.drivelist.Name = "drivelist";
             this.drivelist.Size = new System.Drawing.Size(85, 173);
             this.drivelist.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Font = new System.Drawing.Font("Miriam", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label1.Location = new System.Drawing.Point(419, 337);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 37);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Done";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.drivelist.SelectedIndexChanged += new System.EventHandler(this.drivelist_SelectedIndexChanged);
             // 
             // timer1
             // 
--- PhotoSelectGui/Form1.cs
+++ PhotoSelectGui/Form1.cs
@@ -13,6 +13,7 @@ namespace PhotoSelectGui
     public partial class MainPS : Form
     {        
 
+        // saves groupBox1 exit sliding movement position in each tick
         double x;
         double y;
         public MainPS()
@@ -27,9 +28,11 @@ namespace PhotoSelectGui
 
         private void MainPS_Load(object sender, EventArgs e)
         {
+            // read all drives info and add their 'name' to driveList(ListBox)
             foreach (DriveInfo di in DriveInfo.GetDrives())
                 drivelist.Items.Add(di);
             
+            // saves start position for "DONE" movement
             x = groupBox1.Location.X;
             y = groupBox1.Location.Y;
         }
@@ -39,28 +42,31 @@ namespace PhotoSelectGui
             folderslist.Items.Clear();
             try
             {
+                // display drive root folder content (folders names)
                 DriveInfo drive = (DriveInfo)drivelist.SelectedItem;
                 foreach (DirectoryInfo dirInfo in drive.RootDirectory.GetDirectories())
                     folderslist.Items.Add(dirInfo);
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                // possible reason - Selected drive is the DVD Drive
+                folderslist.Items.Add(ex.Message);                                
             }
         }
 
+        // the delta move for each tick for groupBox1
         private void timer1_Tick(object sender, EventArgs e)
         {
             x=x*1.13;
-                groupBox1.Location = new Point((int)x, (int)y);
+            groupBox1.Location = new Point((int)x, (int)y);
         }
 
+        // DONE Buttom on groupBox1
         private void label1_Click(object sender, EventArgs e)
         {
             timer1.Enabled = true;
 
         }
-
 
     }
 }
