7ee89652f1719deb862f9fcc193d5cdd69855beb
yossida <Yossi@Yossi-Leptop.(none)>
2011-12-12 16:59:20 +0200

    added Task struct for sending the information from the guy to the FeaturesLayer

--- Features/FeaturesLayer.cs
+++ Features/FeaturesLayer.cs
@@ -4,16 +4,34 @@ using System.Linq;
 using System.Text;
 namespace Features
 {
-    public enum Feature { BIT_EXACT = 0, BAD_CONTRAST, SIMILARITY, PARTIAL_BLOCKAGE };
+    public enum Feature { BIT_EXACT = 0, BAD_CONTRAST, SIMILARITY, PARTIAL_BLOCKAGE, NUM_FEATURES };
+    public struct Task
+    {
+        public List<string> ImagePathes
+        {
+            get { return ImagePathes; }
+            set { ImagePathes = value; }
+        }
+        public bool[] Features
+        {
+            get { return Features; }
+            set { Features = value; }
+        }        
+    }
     
     public class FeaturesLayer
     {
-        
+        private Task task;
         //private List<string> pathList;
 
-        public FeaturesLayer()
+        public FeaturesLayer(Task task)
         {
+            this.task = task;
+        }
 
+        public void setTask(Task task)
+        {
+            this.task = task;
         }
 
 
