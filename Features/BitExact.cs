41f9d6f05f9682ad46faa3015efe9cdb2c36bfa5
yossida <Yossi@Yossi-Leptop.(none)>
2011-12-19 11:03:33 +0200

    BitExact documentation

--- Features/BitExact.cs
+++ Features/BitExact.cs
@@ -8,6 +8,11 @@ namespace Features
 {
     class BitExact
     {
+        # region class properties
+
+        // AROUND defines relative error
+        //Propotion defines photo's size type 
+        //checkedImages: true if we find a match otherwise false
         private const double AROUND = 0.05;
         private enum Propotion { p3_4 = 0, p4_3, p9_16, p16_9, pOther };
 
@@ -20,7 +25,12 @@ namespace Features
         private List<List<string>> matches;        
 
         protected BitExactRes results;
+        
+        # endregion class properties
 
+        # region public functions
+
+        //default constructor, set & init variables
         public BitExact()
         {
             matches = new List<List<string>>();
@@ -28,6 +38,7 @@ namespace Features
             numImages = 0;
         }
 
+        //set path of images, and set relevant variables
         public void setImages(ImageInfo[] images)
         {
             this.images = images;
@@ -37,39 +48,46 @@ namespace Features
             propotions = new Propotion[numImages];
         }
 
-        private void initResults()
-        {
-            // first empty result
-            matches = new List<List<string>>();
-            indexList = 0;
-        }
-
+        //this is the struct function that will be returned after the BitExact Feature process is ended
         public BitExactRes getResults()
         {
             return results;
         }
 
+        //function that check matches
         public void run()
         {
 
-            setPropotion();
+            setPropotion(); // set propotion to images
             for (int i = 0; i < numImages; i++)
             {
-                if (checkedImages[i])
+                if (checkedImages[i]) //if we founded match then don't check the image
                     continue;
-                bool isAddedToList = false;
+
+                bool isAddedToList = false; //check if there is match with another image
                 for (int j = i + 1; j < numImages - 1; j++)
-                    if (propotions[i] == propotions[j])
-                        isAddedToList = isAddedToList || EqualImages(i, j);
+                    if (propotions[i] == propotions[j]) //check only images with similar propotions
+                        isAddedToList = isAddedToList || EqualImages(i, j); //equal is true if there is a match
          
-                if (isAddedToList)
+                if (isAddedToList) //if we found match to images[i] count indexList
                     indexList++;
             }
             // set results
             results = new BitExactRes(matches);
         }
 
+        # endregion public functions
 
+        # region private functions
+
+        //empty matches
+        private void initResults()
+        {
+            matches = new List<List<string>>();
+            indexList = 0;
+        }
+
+        // set the propotion for each image
         private void setPropotion()
         {
             for (int i = 0; i < numImages; i++)
@@ -89,6 +107,8 @@ namespace Features
             }
         }
 
+        // check size of images, AROUND the exact point
+        //return true if it similar to wantedProp
         private bool checkPropotion(Bitmap im, double wantedProp)
         {
             if ((im.Height / im.Width <= wantedProp + AROUND) && (im.Height / im.Width >= wantedProp - AROUND))
@@ -96,14 +116,17 @@ namespace Features
             return false;
         }
         
+        //equal images with similar propotion
         private bool EqualImages(int first, int second)
         {
             int size = images[first].getIm().Height + images[first].getIm().Width;
-            int yes = 0;
+            int yes = 0; //count the same bites in first & second
             for (int i = 0; i < size; i++)
                 if (Math.Abs(images[first].getImb()[i] - images[second].getImb()[i]) <= AROUND)
                     yes++;
-            if (yes / size >= 100 - 100 * AROUND)
+
+            //if the success more then (95%) [100 - 100 * ARROUND] the images is eaqual
+            if (yes / size >= 100 - 100 * AROUND) 
             {
                 AddToResult(first, second);
                 return true;
@@ -113,7 +136,7 @@ namespace Features
 
         private void AddToResult(int i, int j)
         {
-
+            //if we founded a match before add j to images i, otherwise add a new list of matches
             if (!checkedImages[i])
             {
                 checkedImages[i] = true;
@@ -125,5 +148,8 @@ namespace Features
 
             checkedImages[j] = true;
         }
+
+
+        # endregion private functions
     }
 }
