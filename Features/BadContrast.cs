ecbba13b9bd70ea8ceec0437342161aa55171545
mayasa <Maya@Maya-PC.(none)>
2012-01-02 17:42:01 +0200

    added bad contrast classes

---
+++ Features/BadContrast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Diagnostics;
+using System.Threading;
+using AForge.Math; // Histogram
+using Bitmap = System.Drawing.Bitmap;
+
+namespace Features
+{
+
+    public class BadContrast
+    {
+        private ImageInfo[] images;
+        private List<string> matches;
+        private int num_images;
+        
+        public BadContrast(ImageInfo [] images)
+        {
+            this.images = images;
+            matches = new List<string>();
+            num_images = images.Length;
+        }
+
+        public void run()
+        {
+            Histogram x = images[0].getHist();
+            double m = x.StdDev;
+            Debug.WriteLine(m);
+        }
+    }
+}
---
+++ FeaturesTests/BadContrastTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Features;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace FeaturesTests
+{
+    class BadContrastTest
+    {
+        public const string PATH = @"C:\Users\Maya\Dropbox\פרויקט - הנדסת תוכנה\badContrast";
+        string[] pathes = Directory.GetFiles(PATH, "*.jpg", SearchOption.AllDirectories);
+
+        [TestMethod]
+        public void TestBadContrast()
+        {
+            int numImages = pathes.Length;
+            ImageInfo[] images = new ImageInfo[numImages];
+            for (int i = 0; i < numImages; i++)
+            {
+                images[i] = new ImageInfo(pathes[i]);
+            }
+
+            BadContrast bc;
+            bc = new BadContrast(images);
+            bc.run();
+        }
+    
+    
+    
+    }
+}
