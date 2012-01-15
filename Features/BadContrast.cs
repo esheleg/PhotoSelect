aa10d87947b5b14633252a721ed0084cb4281d29
danielXr <daniel.braunstein@gmail.com>
2012-01-15 15:16:42 +0200

    isBadContrast function has been extended

--- Features/BadContrast.cs
+++ Features/BadContrast.cs
@@ -35,12 +35,16 @@ namespace Features
                 Histogram hist = images[i].getHist();
                 double mean = hist.Mean;
                 double median = hist.Median;
-
-
-                if (isBadContrast(mean, median))
+                double sd = hist.StdDev;
+               /* Debug.WriteLine(images[i].getPath());
+                Debug.WriteLine("sd is: "+sd);
+                Debug.WriteLine("median is: "+median);
+                Debug.WriteLine("mean is: " +mean);*/
+                if (isBadContrast(mean, median, sd))
                 {
                     matches.Add(images[i].getPath());
                     Debug.WriteLine(images[i].getPath());
+                    //Debug.WriteLine(sd);
                 }
             }
             results = new BadContrastRes(matches);
@@ -51,14 +55,17 @@ namespace Features
             get { return results; }
         }
 
-        private bool isBadContrast(double mean, double median)
+        private bool isBadContrast(double mean, double median, double sd)
         {
-            if (mean < DARK && median < DARK)
+            /*if (mean < DARK && median < DARK)
                 return true;
 
             if (mean > BRIGHT && median > BRIGHT)
+                return true;*/
+            if(sd<=15)
                 return true;
-
+            if ((mean > 200 && median > 200) || (mean < 90 && median < 90))
+                return true;
             return false;
         }
     }
